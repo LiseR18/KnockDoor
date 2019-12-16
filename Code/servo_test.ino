184c586c4f0968ec8daa323651b84ad7796e9774
DevKrazy <ndmroot-git@yahoo.fr>
2019-12-16 15:11:07 +0100

    Ajout support du potentiomètre
    Le programme servo_test fonctionne désormais avec un potentiomètre. Il fait tourner le servomoteur de 0 à 180 degrés.

--- Code/servo_test.ino
+++ Code/servo_test.ino
@@ -1,15 +1,21 @@
 #include <Servo.h>
 
 Servo servo;
-
+const int pot = A0;
 
 void setup() {
-  servo.attach(3);
-  servo.write(180);
-
+  Serial.begin(9600);
+  servo.attach(9);
+  pinMode(pot, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int pot_val = analogRead(pot);
+  Serial.print("pot_val = ");
+  Serial.println(pot_val);
 
+  int servo_val = map(pot_val, 0, 1023, 0, 180);
+  Serial.print("servo_val = ");
+  Serial.println(servo_val);
+  servo.write(servo_val);
 }
