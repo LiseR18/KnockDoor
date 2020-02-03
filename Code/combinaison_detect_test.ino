6d6492930c30c48c353cc3b4ee7a5c41834a25e8
DevKrazy <ndmroot-git@yahoo.fr>
2020-02-03 10:18:58 +0100

    Modif de debug
    Changement de place du code qui affiche dans la console à quel coup on est.

--- Code/combinaison_detect_test.ino
+++ Code/combinaison_detect_test.ino
@@ -1,5 +1,5 @@
 const int button = 7;
-unsigned long ref_time;
+unsigned long ref_time; //temps de référence
 const int max_knock = 10;
 int combinaison[max_knock] = {};
 
@@ -12,24 +12,63 @@ void setup() {
   for (int i = 0; i < max_knock; i++) {
     combinaison[i] = 0;
   }
+  for (int j = 0; j < max_knock; j++) {
+    Serial.println(combinaison[j]);
+  }
+
+  ref_time = millis(); //on initialise le temps de référence
 }
 
+
 void loop() {
-  unsigned long ref_time = millis(); //temps de référence
-  int button_val = digitalRead(button); //valeur du pin du bouton 
-  int knock_count = 0;
+  int knock_count = 0; //nombre de coups reçus
   do { 
-    if (button_val == LOW) { //si le bouton est appuyé
-      //on ajoute à notre tableau le temps entre les 2 coups
-      combinaison[knock_count] = millis() - ref_time; 
-      ref_time = millis(); //le nouveau temps de référence est le temps actuel
-      Serial.println(knock_count);
-      knock_count += 1; //on incrémente notre compteur de coups
+    
+    if (digitalRead(button) == LOW) { //si le bouton est appuyé
+      unsigned long knock_time = millis();
+      
+      Serial.print("BOUTON appuyé à : ");
+      Serial.println(knock_time);
+
+      if (knock_count <= 11) {
+      
+        if (knock_count == 0) { //si c'est  le premier coup tapé
+          ref_time = knock_time;
+        } else {
+          //on ajoute à notre tableau le temps entre les 2 coups
+          combinaison[knock_count - 1] = knock_time - ref_time; 
+          
+          Serial.print("millis - ref_time = ");
+          Serial.print(knock_time);
+          Serial.print(" - ");
+          Serial.print(ref_time);
+          Serial.print(" = ");
+          Serial.println(knock_time - ref_time);
+        }
+
+        Serial.print("COUP NUMERO : ");
+        Serial.println(knock_count);
+        
+        delay(500); //on évite le rebond
+        knock_count += 1; //on incrémente notre compteur de coups
+        ref_time = knock_time;
+        
+      } else {
+        Serial.println("On a dépassé le nombre de coup");
+        break;
+      }
+      
     }
-  } while (millis() - ref_time <= 2000); //tant que l'on n'attend pas plus de 2 secondes
+
+  } while (millis() - ref_time <= 4000); //tant que l'on n'attend pas plus de 2 secondes
+
+  
   knock_count = 0; //on a terminé d'enregistrer la combinaison donc on reset le compteur de coups
+  
   //on affiche la combinaison
+  Serial.println("On est sortis de la boucle, voici la combinaison enregistrée :");
   for (int i = 0; i < max_knock; i++) {
     Serial.println(combinaison[i]);
   }
+  
 }
